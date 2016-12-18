734e7a6c137ebceeb8ad1bb22fe54b8fda0813ac
PENTA <PENTA@VAIO>
2016-12-18 14:12:59 +0900

    Explorer取り込み成功

--- ExplolerEx/MainWindow.xaml.cs
+++ ExplolerEx/MainWindow.xaml.cs
@@ -2,10 +2,17 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-using System.Windows;
-
 namespace ExplolerEx
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using Microsoft.WindowsAPICodePack.Controls;
+    using Microsoft.WindowsAPICodePack.Shell;
+
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
@@ -14,6 +21,13 @@ namespace ExplolerEx
         public MainWindow()
         {
             this.InitializeComponent();
+            this.Loaded += new RoutedEventHandler(this.ExplorerBrowserTestWindow_Loaded);
+        }
+
+        private void ExplorerBrowserTestWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Navigate to initial folder
+            this.eb.ExplorerBrowserControl.Navigate((ShellObject)KnownFolders.Desktop);
         }
     }
 }
